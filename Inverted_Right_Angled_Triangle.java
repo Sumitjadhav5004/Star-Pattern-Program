0ec29839aff91e6829695308b1e7cda432a1212d
sumitjadhav5004 <117936512+Sumitjadhav5004@users.noreply.github.com>
2026-01-12 17:25:03 +0530

    Add Inverted Right Angled Triangle program

---
+++ Inverted_Right_Angled_Triangle.java
@@ -0,0 +1,13 @@
+public class Inverted_Right_Angled_Triangle{
+    public static void main(String[] args) {
+        int n=6;
+
+        for(int i=1;i<=n;i++){
+            for(int j=n;j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
